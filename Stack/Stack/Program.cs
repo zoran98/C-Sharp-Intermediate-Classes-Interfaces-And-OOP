8a47155f0ba63d479886e44561e436d7c0cd4acd
zoran98 <zoranjaksic99988@gmail.com>
2023-12-04 16:39:32 +0100

    Exercise2

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the stack!");
+            var stack = new Stack();
+
+            var input = new object[] { 1, 2, null };
+
+            foreach (var item in input)
+            {
+                if(item != null)
+                {
+                    stack.Push(item);
+                    Console.WriteLine($"added {item.ToString()} to the stack");
+                }
+            }
+
+            //stack.Clear();
+
+            Console.WriteLine(stack.Pop().ToString());
+            Console.WriteLine(stack.Pop().ToString());
+            Console.WriteLine(stack.Pop().ToString());
+            Console.WriteLine(stack.Pop().ToString());
+            Console.WriteLine(stack.Pop().ToString());
+
+
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private List<object> stack {  get; set; }
+
+        public Stack()
+        {
+            stack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            stack.Add(obj);
+            Console.WriteLine("Object added successfully!");
+        }
+
+        public object Pop()
+        {
+            if(stack.Count > 0)
+            {
+                var popedStack = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                return popedStack;
+            }
+            else
+            {
+                Console.WriteLine("No items left to pop.");
+                return -1;
+            }
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+    }
+}
